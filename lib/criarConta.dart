851ea16084f47f849c6ccfefe6b08ada31789070
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-09-21 20:07:41 -0300

    Initial structure to all files

---
+++ lib/criarConta.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CriarConta extends StatefulWidget {
+  const CriarConta({Key? key}) : super(key: key);
+
+  @override
+  State<CriarConta> createState() => _CriarContaState();
+}
+
+class _CriarContaState extends State<CriarConta> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
