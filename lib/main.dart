b6cf9dba35113b32079a5e629471a1d002809455
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-09-21 20:43:35 -0300

    adding firebase core import at main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_firebase/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 
 void main() {
+
+  
   runApp(MaterialApp(
     home: Login(),
     debugShowCheckedModeBanner: false,
