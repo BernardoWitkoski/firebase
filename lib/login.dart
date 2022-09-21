96749eae3392de15de9a1d628f3ebe4b5d409094
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-09-21 20:32:20 -0300

    functions in login class

--- lib/login.dart
+++ lib/login.dart
@@ -8,8 +8,52 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+  TextEditingController _controllerEmail = TextEditingController();
+  TextEditingController _controllerSenha = TextEditingController();
+
+  _validaCampo() {}
+  _logarUsuario() {}
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                // login image
+                Padding(
+                  padding: EdgeInsets.all(5), 
+                  child: TextField(
+                    controller: _controllerEmail,
+                    autofocus: true,
+                    keyboardType: TextInputType.emailAddress,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                      hintText: "Email",
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(5), 
+                  child: TextField(
+                    controller: _controllerSenha,
+                    style: TextStyle(fontSize: 20),
+                    decoration: InputDecoration(
+                      hintText: "Senha",
+                    ),
+                  ),
+                ),
+                ElevatedButton(onPressed: _validaCampo, child: Text("Entrar"))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
