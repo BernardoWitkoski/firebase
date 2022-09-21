fd93c11e0a1ff8f4f6d843da4acbb0311032ba0c
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-09-21 20:48:18 -0300

    setting minSdkVersion to 21 and targetSdkVersion to 30

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,8 @@ import 'package:projeto_firebase/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 
-void main() {
-
-  
+void main() async {
+  await Firebase.initializeApp();
   runApp(MaterialApp(
     home: Login(),
     debugShowCheckedModeBanner: false,
