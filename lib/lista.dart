0f36d360c36a1c5cb6ab26f85dbafe730cca9118
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-10-05 20:03:51 -0300

    Implementing lista

--- lib/lista.dart
+++ lib/lista.dart
@@ -10,10 +10,48 @@ class ListaView extends StatefulWidget {
 class _ListaViewState extends State<ListaView> {
   @override
   Widget build(BuildContext context) {
+    //pegando o tamanho da tela e armazenando em uma variavel
+    var largura = MediaQuery.of(context).size.width;
+    var altura = MediaQuery.of(context).size.height;
+    //pegando o tamanho da AppBar
+    var alturaAppbar = AppBar().preferredSize.height;
+    // subtraindo para termos a area útil
+    var alturaCorpo = altura - alturaAppbar;
+
+
+    var corpo = Container(
+      width: largura,
+      height: alturaCorpo,
+      child: ListView(
+        children: [
+          ListTile(
+            onTap: () {
+              print("AQUI");
+            },
+          title: const Text("Bem vindo!"),
+          subtitle: const Text("Olá <nome usuário>"),  
+          ),
+          ListTile(
+          title: const Text("Bem vindo!"),
+          subtitle: const Text("Olá <nome usuário>"),  
+          ),
+          ListTile(
+          title: const Text("Bem vindo!"),
+          subtitle: const Text("Olá <nome usuário>"),  
+          ),
+          ListTile(
+          title: const Text("Bem vindo!"),
+          subtitle: const Text("Olá <nome usuário>"),  
+          ),
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(),
       body: Container(
-        
+        //montando list view, interface na variavel corpo;
+        child: Column(children: [corpo],)
       ),
     );
   }
